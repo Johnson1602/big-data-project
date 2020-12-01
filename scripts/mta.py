978c8f5e6c5538c23f218b90ec1ae88335782bd8
Ryan_Wang <zw2315@nyu.edu>
2020-12-01 18:16:16 -0500

    mta data process using spark

--- scripts/mta.py
+++ scripts/mta.py
@@ -1,51 +1,65 @@
 import pandas as pd
-import numpy as np
-from datetime import datetime, timedelta
+from datetime import date
 import pyspark
 from pyspark.sql import SparkSession
-from functools import reduce
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import *
+from pyspark.sql.window import Window
 
 spark = SparkSession.builder.getOrCreate()
 
-def reducemta(year):
-    # mta dataset
+def reduceMTA(df, year):
+    df = df.select('C/A', 'UNIT', 'SCP', 'DATE', 'TIME', 'ENTRIES')
+    df = df.withColumn('date2', to_date(unix_timestamp('DATE', 'MM/dd/yyyy').cast('timestamp')))
+
+    # get difference bewteen data
+    column_list = ['C/A', 'UNIT', 'SCP']
+    win_spec = Window.partitionBy([col(x) for x in column_list]).orderBy(['DATE', 'TIME'])
+    df = df.withColumn("entry_diff", df.ENTRIES - lag("ENTRIES", 1).over(win_spec))
+
+    # clean data
     if year == 2019:
-        mta = pd.read_csv('/home/zw2315/project/turnstile_2019.txt', sep = ',',header = None)
+        start_date, end_date = date(2019, 3, 1), date(2019, 10, 31)
     elif year == 2020:
-        mta = pd.read_csv('/home/zw2315/project/turnstile_2019.txt', sep = ',',header = None)
-
-    mta.columns =['C/A','unit','SCP','Station','Linename','division','date','time','desc','entries','exits']
-    # process data
-    mta['datetime'] = pd.to_datetime(mta.date + ' ' + mta.time, format='%m/%d/%Y  %H:%M:%S')
-    mta['turnstile'] = mta['C/A'] + '-' + mta['unit'] + '-' + mta['SCP']
-    mta_sorted = mta.sort_values(['turnstile', 'datetime'])
-    mta_sorted = mta_sorted.reset_index(drop = True)
-
-    # agg dataframe
-    turnstile_grouped = mta_sorted.groupby(['turnstile'])
-    mta_sorted['entries_diff'] = turnstile_grouped['entries'].transform(pd.Series.diff)
-    mta_sorted['exits_diff'] = turnstile_grouped['exits'].transform(pd.Series.diff)
-
-    # clean data 
-    mta_sorted['entries_diff'] = mta_sorted['entries_diff'].fillna(0)
-    mta_sorted['exits_diff'] = mta_sorted['exits_diff'].fillna(0)
-    mta_sorted['entries_diff'][mta_sorted['entries_diff'] < 0] = 0 
-    mta_sorted['exits_diff'][mta_sorted['exits_diff'] < 0] = 0 
-    mta_sorted['entries_diff'][mta_sorted['entries_diff'] >= 6000] = 0 
-    mta_sorted['exits_diff'][mta_sorted['exits_diff'] >= 6000] = 0
-
-    # create spark dataframe
-    mta_processed = mta_sorted[['turnstile','date', 'entries_diff']]
-    mta_spark = spark.createDataFrame(mta_processed)
-    mta_spark = mta_spark.groupBy("date").sum("entries_diff")
+        start_date, end_date = date(2020, 3, 1), date(2020, 10, 31)
+    mask = (df['date2'] >= start_date) & (df['date2'] <= end_date) & (df['entry_diff'] > 0) & (df['entry_diff'] < 6000)
+    df = df[mask]
 
     # output result
-    if year == 2019:
-        mta_spark.select('*').write.save('mta_2019.out', format='csv')
-    elif year == 2020:
-        mta_spark.select('*').write.save('mta_2020.out', format='csv')
+    df = df.groupBy("date2").agg(sum("entry_diff").alias('count'))
 
-reducemta(2019)
-reducemta(2020)
+    return df
+
+# read & select data
+# import covid-19 positive data
+covid = pd.read_csv('/home/wx650/project/data/ny-covid19-positive/ny-covid19-positive.csv')[['date', 'positive', 'positiveIncrease']]
+
+# mta data
+df_19 = (spark.read.format("csv").options(header="true").load("/user/wx650/project/data/mta/turnstile_2019.txt"))
+df_20 = (spark.read.format("csv").options(header="true").load("/user/wx650/project/data/mta/turnstile_2020.txt"))
+
+# process data
+df_19 = reduceMTA(df_19, 2019).orderBy('date2')
+df_20 = reduceMTA(df_20, 2020)
+
+
+# Adding data of March 1st to covid dataset
+covid.loc[271] = ['2020-03-01', 0, 0]
+
+# Convert string to date
+covid['date'] = pd.to_datetime(covid['date']).dt.date
+
+# Find data ranging from 2020-03-01 to 2020-10-31
+start_date, end_date = date(2020, 3, 1), date(2020, 10, 31)
+mask = (covid['date'] >= start_date) & (covid['date'] <= end_date)
+covid = covid[mask].sort_values(by='date')
+
+# Spark COVID-19 datafram from covid
+scovid = spark.createDataFrame(covid)
+
+# Taxi data join with COVID datafram
+df_20 = df_20.join(scovid, df_20['date2'] == scovid['date']).select(scovid['date'], 'count', 'positive').orderBy('date')
+
+# output result
+df_19.select('*').write.save('project/output/mta_2019.out', format='csv')
+df_20.select('*').write.save('project/output/mta_2020_covid.out', format='csv')
